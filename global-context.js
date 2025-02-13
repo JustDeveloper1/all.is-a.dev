4d08399bfb3875b381a32b6124d0c2dcbdf6ea75
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-02-13 14:16:40 +0500

    Convert to Next.js + _just + Github Pages website

---
+++ global-context.js
@@ -0,0 +1,71 @@
+/*
+
+MIT License
+
+Copyright (c) 2025 JustDeveloper <https://justdeveloper.is-a.dev/>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+import { createContext, useMemo, useContext, useState, useEffect } from 'react'
+import { useLocale } from "next-intl";
+
+const GlobalContext = createContext(null)
+
+export const GlobalProvider = ({ initialLocales, children }) => {
+  const localeValue = useLocale()
+  const [locales, setLocales] = useState(initialLocales ?? [{"name":"English","short":"en"}, {"name":"Russian","short":"ru"}])
+  const [locale, setLocale] = useState({"name":"English","short":"en"})
+  
+  useEffect(() => {
+    if (!locales) {
+      return
+    }
+
+    const currentLangValue = locales.find((el) => el.short === localeValue)
+    setLocale(currentLangValue)
+  }, [locales])
+
+  const value = useMemo(() => {
+    return {
+      locales,
+      locale,
+      setLocales,
+      setLocale
+    }
+  }, [locales, locale])
+
+  return (
+    <GlobalContext.Provider value={value}>
+      {children}
+    </GlobalContext.Provider>
+  )
+}
+
+export const useGlobalContext = () => {
+  const context = useContext(GlobalContext)
+  if (!context) {
+    throw new Error('useGlobalContext must be used within a GlobalProvider')
+  }
+
+  return {
+    ...context
+  }
+}
---
+++ pages/404.js
@@ -0,0 +1,105 @@
+/*
+
+MIT License
+
+Copyright (c) 2025 JustDeveloper <https://justdeveloper.is-a.dev/>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+// this is unused thing that next.js need anyway ¯\_(ツ)_/¯
+
+import React from 'react'
+import Head from 'next/head'
+
+import { useTranslations } from 'next-intl'
+
+const NotFound = (props) => {
+  return (
+    <>
+      <div className="not-found-container1">
+        <Head>
+          <title>404 - Not Found</title>
+        </Head>
+        <h3>OOPS! PAGE NOT FOUND</h3>
+        <div className="not-found-container2">
+          <h1 className="not-found-text2">404</h1>
+        </div>
+        <div className="not-found-container3">
+          <h2 className="not-found-text3">
+            WE ARE SORRY, BUT THE PAGE YOU REQUESTED WAS NOT FOUND
+          </h2>
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .not-found-container1 {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+            justify-content: center;
+          }
+          .not-found-container2 {
+            display: flex;
+            position: relative;
+            align-items: center;
+            flex-direction: column;
+            justify-content: center;
+          }
+          .not-found-text2 {
+            color: rgb(38, 38, 38);
+            font-size: 252px;
+            margin-top: -20px;
+            font-weight: 900;
+            margin-bottom: -20px;
+            letter-spacing: -20px;
+          }
+          .not-found-container3 {
+            width: 421px;
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            justify-content: center;
+          }
+          .not-found-text3 {
+            text-align: center;
+            font-weight: 400;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default NotFound
+
+export async function getStaticProps(context) {
+  const messages = (await import('/locales/' + context.locale + '.json'))
+    .default
+  return {
+    props: {
+      messages,
+      ...context,
+    },
+  }
+}
