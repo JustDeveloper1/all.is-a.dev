d0ef68f0534d53e51c65d7638c54ce3d84eba351
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-02-14 23:10:53 +0500

    Updates

--- pages/index.js
+++ pages/index.js
@@ -121,7 +121,7 @@ const Home = () => {
     fetchData();
   }, []);
 
-  const subdomain = (item) => {
+  const subdomain = (item, listId) => {
     const domain = item.domain;
     let description = item.description;
     const owner = item.owner.username || item.owner || err.noOwner2;
@@ -136,9 +136,10 @@ const Home = () => {
     let title = description; 
     let tag1 = 'span';
     let ptag2 = 'div';
-    let tag2p = `class="subdomain-link" id="subdomain-${id}" title="${description}"`
+    let tag2p = `class="subdomain-link" id="subdomain-${id}" title="${description}"`;
+    const isOff = isOfficial(domain);
     if (spotlight) {tag1 = 'span id="spotlight"'}
-    if (isOfficial(domain)) {
+    if (isOff) {
       ptag2 = 'a';
       tag2p = `class="subdomain-link" target="_blank" title="${description}" href="${link}"`;
       if (domain === "@.is-a.dev") {
@@ -147,29 +148,84 @@ const Home = () => {
     }
     const tag2 = `${ptag2} ${tag2p}`;
     let sdinfo = title !== domain ? `<div class="subdomain-info" id="info-${id}">(${domain}) <small>#${id}</small></div>` : `<div class="subdomain-info" id="info-${id}"><small>#${id}</small></div>`;
-    let output = `<${tag1}><${tag2}>${title}${sdinfo}</${ptag2}><info> by <a target="_blank" href="https://github.com/${profile.replace('@', '')}" title="${owner} on GitHub">${owner}</a></info></span>`;
+    let output = `<${tag1}><${tag2}>${truncateString2(title, 50)}${sdinfo}</${ptag2}><info> by <a target="_blank" href="https://github.com/${profile.replace('@', '')}" title="${owner} on GitHub">${owner}</a></info></span>`;
+    const isSystem = isSys(domain);
+    if (listId == 1 && !isOff || listId == 3 && !isSystem || listId == 2 && (isOff || isSystem)) {
+      output = '';
+    }
     return output
   }
 
   return (
-    <div>
-      <h1>Subdomains</h1>
+    <>
+      <div class="NOT-A DISCLAIMER THIS-IS-HEADER">
+        <img alt="Domains Count" src="https://img.shields.io/github/directory-file-count/is-a-dev/register/domains?color=6e3bf3&amp;label=domains&amp;style=for-the-badge" width="106" height="31" />
+        <h1>
+          js.is-a.dev
+        </h1>
+        <h1>
+          all.is-a.dev
+        </h1>
+        <h1>
+          jd.is-a.dev
+        </h1>
+        <p>
+          Every website on .is-a.dev
+        </p>
+      </div>
+      <div className="DISCLAIMER">
+        <h1>DISCLAIMER</h1>
+        <p>This website is <strong style={{ textDecoration: 'underline', padding: '0px !important' }}>NOT</strong> claiming to be an official website/subdomain of is-a.dev. This is just a visualized version of <a href="https://raw-api.is-a.dev/" target="_blank">raw-api.is-a.dev</a>.</p>
+      </div>
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <ul>
-          {listItems.map(item => (
-            <li key={item.id}
-              dangerouslySetInnerHTML={{
-                __html:
-                  subdomain(item),
-              }}
-            >
-            </li>
-          ))}
-        </ul>
+        <>
+          <p>Official Subdomains <small>(Check <a href="https://docs.is-a.dev/" target="_blank" title="is-a.dev documentation">https://docs.is-a.dev/</a>)</small></p>
+          <ul>
+            {listItems.map(item => {
+              const sbdItem = subdomain(item, 1);
+              if (sbdItem !== '') return (
+              <li key={item.id}
+                dangerouslySetInnerHTML={{
+                  __html:
+                    sbdItem,
+                }}
+              >
+              </li>
+            )})}
+          </ul>
+          <p>Subdomains</p>
+          <ul>
+            {listItems.map(item => {
+              const sbdItem = subdomain(item, 2);
+              if (sbdItem !== '') return (
+              <li key={item.id}
+                dangerouslySetInnerHTML={{
+                  __html:
+                    sbdItem,
+                }}
+              >
+              </li>
+            )})}
+          </ul>
+          <p>Subdomains that are most likely made for verifications</p>
+          <ul>
+            {listItems.map(item => {
+              const sbdItem = subdomain(item, 3);
+              if (sbdItem !== '') return (
+              <li key={item.id}
+                dangerouslySetInnerHTML={{
+                  __html:
+                    sbdItem,
+                }}
+              >
+              </li>
+            )})}
+          </ul>
+        </>
       )}
-    </div>
+    </>
   );
 };
 
