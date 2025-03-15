9a2935fa59d7f8cdbb37259c1a821087579756c0
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-03-15 14:36:51 +0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -24,66 +24,91 @@ SOFTWARE.
 
 */
 
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { useTranslations } from 'next-intl';
-
-const CustomDocument = () => {
-  const translate = useTranslations();
-
-  return (
-    <Html>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-        <meta charSet="utf-8"></meta>
-        <meta property="twitter:card" content="summary_large_image"></meta>
-        <style dangerouslySetInnerHTML={{ __html: 'html { line-height: 1.15;}body { margin: 0;}* { box-sizing: border-box; border-width: 0; border-style: solid; -webkit-font-smoothing: antialiased;}p,li,ul,pre,div,h1,h2,h3,h4,h5,h6,figure,blockquote,figcaption { margin: 0; padding: 0;}button { background-color: transparent;}button,input,optgroup,select,textarea { font-family: inherit; font-size: 100%; line-height: 1.15; margin: 0;}button,select { text-transform: none;}button,[type="button"],[type="reset"],[type="submit"] { -webkit-appearance: button; color: inherit;}button::-moz-focus-inner,[type="button"]::-moz-focus-inner,[type="reset"]::-moz-focus-inner,[type="submit"]::-moz-focus-inner { border-style: none; padding: 0;}button:-moz-focus,[type="button"]:-moz-focus,[type="reset"]:-moz-focus,[type="submit"]:-moz-focus { outline: 1px dotted ButtonText;}a { color: inherit; text-decoration: inherit;}input { padding: 2px 4px;}img { display: block;}details { display: block; margin: 0; padding: 0;}summary::-webkit-details-marker { display: none;}[data-thq="accordion"] [data-thq="accordion-content"] { max-height: 0; overflow: hidden; transition: max-height 0.3s ease-in-out; padding: 0;}[data-thq="accordion"] details[data-thq="accordion-trigger"][open] + [data-thq="accordion-content"] { max-height: 1000vh;}details[data-thq="accordion-trigger"][open] summary [data-thq="accordion-icon"] { transform: rotate(180deg);}html { scroll-behavior: smooth }' }} data-tag="reset-style-sheet"></style>
-        <style dangerouslySetInnerHTML={{ __html: 'html { font-family: Inter; font-size: 16px; } body { font-weight: 400; font-style:normal; text-decoration: none; text-transform: none; letter-spacing: normal; line-height: 1.15; color: var(--dl-color-theme-neutral-dark); background: var(--dl-color-theme-neutral-light); fill: var(--dl-color-theme-neutral-dark); }' }} data-tag="default-style-sheet"></style>
-        <link rel="shortcut icon" href="/favicon-32x32.png" type="icon/png" sizes="32x32"></link>
-        <title>all.is-a.dev</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest"></link>
-        <meta name="title" content="all.is-a.dev" />
-        <meta name="keywords" content={translate.raw('meta')} />
-        <meta name="description" content={translate.raw('desc')} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="http://all.is-a.dev/" />
-        <meta property="og:title" content="all.is-a.dev" />
-        <meta property="og:description" content={translate.raw('desc')} />
-        <meta property="og:image" content="https://img.juststudio.is-a.dev/a.png" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta charset="utf-8" />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="vk:image" content="https://img.juststudio.is-a.dev/b.png" />
-        <div dangerouslySetInnerHTML={{ __html: `<script type='application/ld+json'>
-          {
-            "@context": "http://schema.org",
-            "@type": "BreadcrumbList",
-            "itemListElement": [
-              {
-              "@type": "ListItem",
-              "position": "1",
-              "name": "is-a.dev",
-              "item": "https://is-a.dev/"
-            },
-              {
-              "@type": "ListItem",
-              "position": "2",
-              "name": "all.is-a.dev",
-              "item": "https://all.is-a.dev/"
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+class CustomDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          ></meta>
+          <meta charSet="utf-8"></meta>
+          <meta property="twitter:card" content="summary_large_image"></meta>
+          <style
+            dangerouslySetInnerHTML={{
+              __html:
+                'html {  line-height: 1.15;}body {  margin: 0;}* {  box-sizing: border-box;  border-width: 0;  border-style: solid;  -webkit-font-smoothing: antialiased;}p,li,ul,pre,div,h1,h2,h3,h4,h5,h6,figure,blockquote,figcaption {  margin: 0;  padding: 0;}button {  background-color: transparent;}button,input,optgroup,select,textarea {  font-family: inherit;  font-size: 100%;  line-height: 1.15;  margin: 0;}button,select {  text-transform: none;}button,[type="button"],[type="reset"],[type="submit"] {  -webkit-appearance: button;  color: inherit;}button::-moz-focus-inner,[type="button"]::-moz-focus-inner,[type="reset"]::-moz-focus-inner,[type="submit"]::-moz-focus-inner {  border-style: none;  padding: 0;}button:-moz-focus,[type="button"]:-moz-focus,[type="reset"]:-moz-focus,[type="submit"]:-moz-focus {  outline: 1px dotted ButtonText;}a {  color: inherit;  text-decoration: inherit;}input {  padding: 2px 4px;}img {  display: block;}details {  display: block;  margin: 0;  padding: 0;}summary::-webkit-details-marker {  display: none;}[data-thq="accordion"] [data-thq="accordion-content"] {  max-height: 0;  overflow: hidden;  transition: max-height 0.3s ease-in-out;  padding: 0;}[data-thq="accordion"] details[data-thq="accordion-trigger"][open] + [data-thq="accordion-content"] {  max-height: 1000vh;}details[data-thq="accordion-trigger"][open] summary [data-thq="accordion-icon"] {  transform: rotate(180deg);}html { scroll-behavior: smooth  }',
+            }}
+            data-tag="reset-style-sheet"
+          ></style>
+          <style
+            dangerouslySetInnerHTML={{
+              __html:
+                '\n  html {\n    font-family: Inter;\n    font-size: 16px;\n  }\n\n  body {\n    font-weight: 400;\n    font-style:normal;\n    text-decoration: none;\n    text-transform: none;\n    letter-spacing: normal;\n    line-height: 1.15;\n    color: var(--dl-color-theme-neutral-dark);\n    background: var(--dl-color-theme-neutral-light);\n    \n    fill: var(--dl-color-theme-neutral-dark);\n  }\n\n  \n\n  ',
+            }}
+            data-tag="default-style-sheet"
+          ></style>
+          <link
+            rel="shortcut icon"
+            href="/favicon-32x32.png"
+            type="icon/png"
+            sizes="32x32"
+          ></link>
+          <title>all.is-a.dev</title>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest"></link>
+          <meta name="title" content="all.is-a.dev" />
+          <meta name="keywords" content="is-a-dev, all, is, a, dev, every, free, subdomain, domain, бесплатно, бесплатный, субдомен, домен, субдоменное имя, доменное имя, поддомен, поддоменное имя, бесплатное, имя" />
+          <meta name="description" content="a System Development Studio" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="http://all.is-a.dev/" />
+          <meta property="og:title" content="all.is-a.dev" />
+          <meta property="og:description" content="Every subdomain on .is-a.dev" />
+          <meta
+            property="og:image"
+            content="https://img.juststudio.is-a.dev/a.png"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta charset="utf-8" />
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="vk:image" content="https://img.juststudio.is-a.dev/b.png" />
+          <div dangerouslySetInnerHTML={{__html:`<script type='application/ld+json'>
+            {
+              "@context": "http://schema.org",
+              "@type": "BreadcrumbList",
+              "itemListElement": [
+                {
+                "@type": "ListItem",
+                "position": "1",
+                "name": "is-a.dev",
+                "item": "https://is-a.dev/"
+              },
+                {
+                "@type": "ListItem",
+                "position": "2",
+                "name": "all.is-a.dev",
+                "item": "https://all.is-a.dev/"
+              }
+              ]
             }
-            ]
-          }
-        </script>` }} />
-      </Head>
-      <body>
-        <Main></Main>
-        <NextScript></NextScript>
-        <div dangerouslySetInnerHTML={{ __html: '<script type="text/javascript">\n    document.ondragstart = noselect;\n    document.onselectstart = noselect;\n    document.oncontextmenu = noselect;\n    function noselect() {return false;}\n</script>' }}></div>
-      </body>
-    </Html>
-  );
-};
-
-export default CustomDocument;
+          </script>`}} />
+        </Head>
+        <body>
+          <Main></Main>
+          <NextScript></NextScript>
+          <div
+            dangerouslySetInnerHTML={{
+              __html:
+                '<script type="text/javascript">\n    document.ondragstart = noselect;\n    document.onselectstart = noselect;\n    document.oncontextmenu = noselect;\n    function noselect() {return false;}\n</script>',
+            }}
+          ></div>
+        </body>
+      </Html>
+    )
+  }
+}
+export default CustomDocument
